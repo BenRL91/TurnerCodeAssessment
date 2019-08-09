325e3edf154dc909bddd222a2beb15b4b957354a
Ben Lee <benlee.eelned@gmail.com>
2019-08-09 19:42:32 -0400

    CSS grid for details

--- src/Components/DetailPanel.js
+++ src/Components/DetailPanel.js
@@ -9,19 +9,53 @@ export default class DetailPanel extends Component {
   }
 
   //Close the details
-  hideDetails(result){
-    this.props.hideDetailHandler(result);
+  hideDetails(){
+    this.props.hideDetailHandler();
+  }
+
+  getAwardKey(award){
+    if(award.Participants){
+      return `${award.AwardCompany}-${award.Award}-${award.AwardYear}-${award.Participants[0]}`
+    }else {
+      return `${award.AwardCompany}-${award.Award}-${award.AwardYear}`
+    }
+  }
+
+  getDirector(participants){
+    return participants.filter(p => {
+      return p.RoleType == 'Director';
+    })
   }
 
   render() {
     const { result, hideDetailHandler } = this.props
-
+    console.log(result)
     return (
       result &&
-      <div className="details">
-        <h5>{result.TitleName}</h5>
-        <h6>{result.ReleaseYear}</h6>
+      <div className='details'>
+        <button className='close' onClick={ e => this.hideDetails() }>X</button>
+        <h5 className="title">{ result.TitleName }</h5>
+        <h6 className="year">{ result.ReleaseYear }</h6>
+        <h6 className="director">Directed by: { this.getDirector(result.Participants)[0].Name }</h6>
+        <ul className="genres">
+          { result.Genres.map( genre => { return <li key={genre}>{ genre } </li> }) }
+        </ul>
+        <ul className="actors">
+          { result.Participants.map( participant => { return <li key={ `${participant.ParticipantId}-${participant.RoleType}` }>{ participant.Name } </li> }) }
+        </ul>
+        <ul className="others">
+          { result.Participants.map( participant => { return <li key={ `${participant.ParticipantId}-${participant.RoleType}` }>{ participant.Name } </li> }) }
+        </ul>
+        {
+          result.Awards && <ul className="awards">
+                    { result.Awards.map( award => { return <li key={this.getAwardKey(award)}>{ award.Award } </li> }) }
+                  </ul>
+        }
       </div>
     );
   }
 }
+
+/*
+Awards, Genres, Participants -> RoleType, ReleaseYear, TitleName,
+*/
